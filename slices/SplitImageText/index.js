8148fc8cbcc9234d104c526a3b289aafbcc8e1eb
juanmr8 <info@juanmoraromero.com>
2023-08-21 18:11:08 -0300

    first commit

--- slices/SplitImageText/index.js
+++ slices/SplitImageText/index.js
@@ -20,18 +20,14 @@ const SplitImageText = ({ slice }) => {
       <div
         className={`${
           slice.variation === "heroSection" ? "pt-12 pb-24" : ""
-        } flex justify-center items-center w-screen ${
-          slice.primary.background_color
-            ? backgroundColor[slice.primary.background_color]
-            : backgroundColor.Grey
-        }`}>
+        } flex justify-center items-center w-screen`}>
         <div className='container grid grid-cols-1 md:grid-cols-2 gap-24 items-center min-h-[512px] py-24'>
-          {/* <PrismicNextImage
+          <PrismicNextImage
             field={slice.primary.image}
             className={`w-full h-auto rounded-3xl ${
               ["default"].includes(slice.variation) ? "" : "md:order-last"
             }`.trim()}
-          /> */}
+          />
           <div className='flex flex-col gap-4 items-start'>
             {["heroSection"].includes(slice.variation) ? (
               <div>
