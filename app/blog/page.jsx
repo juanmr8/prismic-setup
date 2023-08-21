32b5dd69b5ea5b41aac09975bda740de5ef774ac
juanmr8 <info@juanmoraromero.com>
2023-08-21 16:59:53 -0300

    first commit

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,14 +1,3 @@
-import TextCenter from "@/components/TextCenter";
-import CallToActionButton from "@/components/CallToActionButton";
-import SplitImageLeft from "@/components/SplitImageLeft";
-import SplitButtonLeft from "@/components/SplitButtonLeft";
-import Hero from "@/components/Hero";
-import { createClient } from "@/prismicio";
-import { SliceZone } from "@prismicio/react";
-import { components } from "slices/.";
-
-import * as prismic from "@prismicio/client";
-
 export default async function Blog() {
   return (
     <main>
