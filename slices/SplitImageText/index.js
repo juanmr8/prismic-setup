a4c7bfcb24b8334787ce58c8f59d59214a21d5ff
juanmr8 <info@juanmoraromero.com>
2023-08-21 18:26:36 -0300

    first commit

--- slices/SplitImageText/index.js
+++ slices/SplitImageText/index.js
@@ -1,6 +1,7 @@
 import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
 import ButtonPrismic from "@/components/ButtonPrismic";
+import Button from "@/components/Button";
 
 /**
  * @typedef {import("@prismicio/client").Content.SplitImageTextSlice} SplitImageTextSlice
@@ -20,12 +21,18 @@ const SplitImageText = ({ slice }) => {
       <div
         className={`${
           slice.variation === "heroSection" ? "pt-12 pb-24" : ""
-        } flex justify-center items-center w-screen`}>
+        } flex justify-center items-center w-screen ${
+          slice.primary.background_color
+            ? backgroundColor[slice.primary.background_color]
+            : backgroundColor.Grey
+        }`}>
         <div className='container grid grid-cols-1 md:grid-cols-2 gap-24 items-center min-h-[512px] py-24'>
-          <PrismicNextImage
+          {/* <PrismicNextImage
             field={slice.primary.image}
-            className={`w-full h-auto rounded-3xl`}
-          />
+            className={`w-full h-auto rounded-3xl ${
+              ["default"].includes(slice.variation) ? "" : "md:order-last"
+            }`.trim()}
+          /> */}
           <div className='flex flex-col gap-4 items-start'>
             {["heroSection"].includes(slice.variation) ? (
               <div>
@@ -35,10 +42,11 @@ const SplitImageText = ({ slice }) => {
             ) : (
               <PrismicRichText field={slice.primary.text} />
             )}
-            {/* <ButtonPrismic
+            <Button
               link={slice.primary.button_link}
               label={slice.primary.button_label}
-            /> */}
+              white={false}
+            />
           </div>
         </div>
       </div>
